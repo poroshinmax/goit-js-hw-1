b9fcf982acd7a6512109bcdaabc686907f99678b
Poroshyn Max <poroshinmax2302@gmail.com>
2022-08-09 00:57:08 +0300

    js-hw-1-36

--- js/script.js
+++ js/script.js
@@ -363,3 +363,287 @@
 // console.log(checkPassword("jqueryismyjam"));
 // console.log(checkPassword("angul4r1sl1f3"));
 // console.log(checkPassword("r3actsux"));
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//   switch (type) {
+//     case "starter":
+//       type === "starter"; // Change this line
+//       price = 0; // Change this line
+//       break;
+
+//     case "professional":
+//       type === "professional"; // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case "organization":
+//       type === "organization"; // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+// console.log(getSubscriptionPrice("professional"));
+// console.log(getSubscriptionPrice("organization"));
+// console.log(getSubscriptionPrice("starter"));
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+//   switch (password) {
+//     case null:
+//       message = "Canceled by user!";
+//       break;
+//     case ADMIN_PASSWORD:
+//       message = "Welcome!";
+//       break;
+//     default:
+//       message = "Access denied, wrong password!";
+//       break;
+//   }
+//     if (password === null) {
+//       message = "Canceled by user!";
+//     } else if (password === ADMIN_PASSWORD) {
+//       message = "Welcome!";
+//     } else {
+//       message = "Access denied, wrong password!";
+//     }
+
+//   Change code above this line
+//   return message;
+// }
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword(null));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+// function getShippingCost(country) {
+//   let message;
+
+//   // Change code below this line
+//   switch (country) {
+//     case "China":
+//       message =
+//         "Shipping to China will cost 100 credits";
+//       break;
+//     case "Chile":
+//       message =
+//         "Shipping to Chile will cost 250 credits";
+//       break;
+//     case "Australia":
+//       message =
+//         "Shipping to Australia will cost 170 credits";
+//       break;
+//     case "Jamaica":
+//       message =
+//         "Shipping to Jamaica will cost 120 credits";
+//       break;
+//     default:
+//       message =
+//         "Sorry, there is no delivery to your country";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+// console.log(getShippingCost("Australia"));
+// console.log(getShippingCost("Germany"));
+// console.log(getShippingCost("China"));
+// console.log(getShippingCost("Chile"));
+// console.log(getShippingCost("Jamaica"));
+// console.log(getShippingCost("Sweden"));
+// function getShippingCost(country) {
+//   let message;
+//   let countryName;
+//   let price;
+
+//   // Change code below this line
+//   switch (country) {
+//     case "China":
+//       countryName = "China";
+//       price = 100;
+//       message = `Shipping to ${countryName} will cost ${price} credits`;
+//       break;
+//     case "Chile":
+//       countryName = "Chile";
+//       price = 250;
+//       message = `Shipping to ${countryName} will cost ${price} credits`;
+//       break;
+//     case "Australia":
+//       countryName = "Australia";
+//       price = 170;
+//       message = `Shipping to ${countryName} will cost ${price} credits`;
+//       break;
+//     case "Jamaica":
+//       countryName = "Jamaica";
+//       price = 120;
+//       message = `Shipping to ${countryName} will cost ${price} credits`;
+//       break;
+//     default:
+//       message =
+//         "Sorry, there is no delivery to your country";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+// console.log(getShippingCost("Australia"));
+// console.log(getShippingCost("Germany"));
+// console.log(getShippingCost("China"));
+// console.log(getShippingCost("Chile"));
+// console.log(getShippingCost("Jamaica"));
+// console.log(getShippingCost("Sweden"));
+
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+//   return message;
+// }
+// console.log(getNameLength("Poly"));
+// console.log(getNameLength("Harambe"));
+// console.log(getNameLength("Billy"));
+// console.log(getNameLength("Joe"));
+
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement =
+//   courseTopic[courseTopic.length - 1];
+
+// // Change code above this line
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Change this line
+
+//   return substring;
+// }
+// console.log(getSubstring("Hello world", 3));
+// console.log(getSubstring("Hello world", 6));
+// console.log(getSubstring("Hello world", 8));
+// console.log(getSubstring("Hello world", 11));
+// console.log(getSubstring("Hello world", 0));
+
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+//   result =
+//     message.length <= maxLength
+//       ? message
+//       : message.slice(0, maxLength) + "...";
+//   // Change code above this line
+//   return result;
+// }
+// console.log(
+//   formatMessage("Curabitur ligula sapien", 16)
+// );
+// console.log(
+//   formatMessage("Curabitur ligula sapien", 23)
+// );
+// console.log(
+//   formatMessage(
+//     "Vestibulum facilisis purus nec",
+//     20
+//   )
+// );
+// console.log(
+//   formatMessage(
+//     "Vestibulum facilisis purus nec",
+//     30
+//   )
+// );
+// console.log(
+//   formatMessage(
+//     "Nunc sed turpis a felis in nunc fringilla",
+//     15
+//   )
+// );
+// console.log(
+//   formatMessage(
+//     "Nunc sed turpis a felis in nunc fringilla",
+//     41
+//   )
+// );
+
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase(); // Change this line
+
+//   return normalizedInput;
+// }
+// console.log(normalizeInput("Hello world"));
+// console.log(normalizeInput("This ISN'T SpaM"));
+// console.log(normalizeInput("Big SALE"));
+
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Change this line
+//   return result;
+// }
+// console.log(
+//   checkForName("Egor Kolbasov", "Egor")
+// );
+// console.log(
+//   checkForName("Egor Kolbasov", "egor")
+// );
+// console.log(
+//   checkForName("Egor Kolbasov", "egOr")
+// );
+// console.log(
+//   checkForName("Egor Kolbasov", "Zhenya")
+// );
+// console.log(
+//   checkForName("Vadim Nekrasov", "Vadim")
+// );
+// console.log(
+//   checkForName("Vadim Nekrasov", "vadim")
+// );
+// console.log(
+//   checkForName("Vadim Nekrasov", "Dima")
+// );
+
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+//   message = message.toLowerCase();
+//   result =
+//     message.includes("spam") ||
+//     message.includes("sale");
+
+//   // Change code above this line
+//   return result;
+// }
+// console.log(
+//   checkForSpam("Latest technology news")
+// );
+// console.log(
+//   checkForSpam("JavaScript weekly newsletter")
+// );
+// console.log(
+//   checkForSpam("Get best sale offers now!")
+// );
+// console.log(
+//   checkForSpam("Amazing SalE, only tonight!")
+// );
+
+// console.log(
+//   checkForSpam(
+//     "Trust me, this is not a spam message"
+//   )
+// );
+// console.log(
+//   checkForSpam(
+//     "Get rid of sPaM emails. Our book in on sale!"
+//   )
+// );
+// console.log(
+//   checkForSpam("[SPAM] How to earn fast money?")
+// );
